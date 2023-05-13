2ae5aeb2e4344c59cbf17ba9efb3cc42aeb41096
Arthur Bueno <119634817+ArthurBueno07@users.noreply.github.com>
2023-05-12 21:36:32 -0300

    Add files via upload

--- dart-master/atividades/atvd005/2.dart
+++ dart-master/atividades/atvd005/2.dart
@@ -2,36 +2,33 @@
 import 'dart:io';
 
 // Criação de um Map que armazena os dados dos usuários cadastrados.
-Map<String, dynamic> cadastros = {};
+Map<var, String> musicas = {};
 
 // Função que realiza o cadastro de um usuário.
-void cadastro(nome, cpf, telefone) {
+void cadastroMusicas(nome, cantor, ano) {
   // Adicionando os dados do usuário ao Map.
-  cadastros[nome] = {
-    'CPF': cpf,
-    'Telefone': telefone,
-  };
+  musicas[nome] = {'Cantor': cantor, 'Lançamento': ano} as String;
 
   // Exibindo as informações do usuário cadastrado.
   print('');
-  print('Nome: $nome');
+  print('Nome: $nome ');
   print('=' * 70);
-  print('CPF: $cpf');
+  print('Cantor: $cantor ');
   print('=' * 70);
-  print('Telefone: $telefone');
+  print('Lançamento: $ano ');
   print('=' * 70);
   print('Você foi cadastrado!');
   print('');
 }
 
 // Função que realiza a remoção de um usuário cadastrado.
-dynamic remover(dados, Map cadastros) {
+dynamic remover(dados, Map musicas) {
   // Solicitando o nome do usuário a ser removido.
   stdout.write('Qual usuário você deseja remover: ');
   dynamic nome_remove = stdin.readLineSync()!;
 
   // Removendo o usuário do Map.
-  cadastros.remove(nome_remove);
+  musicas.remove(nome_remove);
 
   // Exibindo a mensagem de cadastro atualizado.
   print('=' * 70);
@@ -43,20 +40,20 @@ dynamic remover(dados, Map cadastros) {
 }
 
 // Função que realiza a consulta de um usuário pelo nome.
-consultarNome(Map<String, dynamic> cadastros, {String? nome_consultar}) {
+consultarMusica(Map musica, {String? musica_consultar}) {
   // Verificando se o nome do usuário foi informado como parâmetro.
-  if (nome_consultar == null) {
+  if (musica_consultar == null) {
     // Solicitando o nome do usuário a ser consultado.
     print('');
     stdout.write('Digite o nome do usuário que deseja buscar: ');
     print('');
-    nome_consultar = stdin.readLineSync();
+    musica_consultar = stdin.readLineSync();
   }
 
   // Verificando se o usuário existe no Map.
-  if (cadastros.containsKey(nome_consultar)) {
+  if (musica.containsKey(musica_consultar)) {
     // Retornando os dados do usuário encontrado.
-    return cadastros[nome_consultar];
+    return musica[musica_consultar];
   } else {
     // Retornando null caso o usuário não exista.
     return null;
@@ -64,15 +61,15 @@ consultarNome(Map<String, dynamic> cadastros, {String? nome_consultar}) {
 }
 
 // Função anônima que exibe as informações de todos os usuários cadastrados.
-contaUsuario() {
+contaMusica() {
   // Verificando se o Map está vazio.
-  if (cadastros.isEmpty) {
+  if (musicas.isEmpty) {
     print('Nenhum usuário cadastrado!');
   } else {
     // Exibindo as informações de todos os usuários cadastrados.
-    print('\nDados cadastrados de todos os usuários:');
+    print('\nMusicas cadastradas: ');
     int quantidade = 0;
-    cadastros.forEach((chave, valor) {
+    musicas.forEach((chave, valor) {
       quantidade += 1;
       print('$quantidade - $chave: $valor');
       print('-' * 70);
@@ -86,6 +83,7 @@ contaUsuario() {
 // Função principal que controla o fluxo do programa.
 void main() {
   // Declaração de variáveis.
+
   var dados;
   bool sair = false;
 
@@ -93,10 +91,10 @@ void main() {
   while (!sair) {
     // Exibindo o menu de opções.
     print('''
-    1 - Para cadastrar usuário
-    2 - Para exibir todos os cadastrados
-    3 - Remover cadastro
-    4 - Buscar usuário
+    1 - Para cadastrar uma musica
+    2 - Para exibir todos as musicas
+    3 - Remover musica
+    4 - Buscar musica
     0 - Sair do programa
     ''');
 
@@ -106,43 +104,40 @@ void main() {
 
     switch (opcao) {
       case 1:
-        stdout.write('Digite seu nome: ');
+        stdout.write('Digite o nome da música: ');
         String nome = stdin.readLineSync()!;
 
-        stdout.write('Digite seu CPF: ');
-        int cpf = int.parse(stdin.readLineSync()!);
-
-        stdout.write('Digite seu telefone: ');
-        int telefone = int.parse(stdin.readLineSync()!);
+        stdout.write('Digite o nome do cantor: ');
+        int cantor = int.parse(stdin.readLineSync()!);
 
         // Chama a função de cadastro para adicionar o usuário ao Map.
-        cadastro(nome, cpf, telefone);
+        cadastroMusicas(nome, cantor, ano);
 
         break;
 
       case 2:
         // Chama a função para exibir todos os usuários cadastrados.
-        contaUsuario();
+        contaMusica();
 
         break;
 
       case 3:
         // Chama a função de remoção passando o Map de cadastros.
-        remover(dados, cadastros);
+        remover(dados, musicas);
         break;
 
       case 4:
-        if (cadastros.isEmpty) {
+        if (musicas.isEmpty) {
           print('');
           print('Não contém nenhum usuário! ');
           print('');
           break;
         }
         // Chama a função de consulta de usuário passando o Map de cadastros.
-        dynamic usuarioEncontrado = consultarNome(cadastros);
-        if (usuarioEncontrado != null) {
+        dynamic musicaEncontrada = consultarMusica(musicas);
+        if (musicaEncontrada != null) {
           print('');
-          print('Usuário encontrado: $usuarioEncontrado');
+          print('Música não encontrada: $musicaEncontrada');
           print('');
         } else {
           print('Usuário não encontrado.');
@@ -191,6 +186,3 @@ void main() {
     }
   }
 }
-
-
-
